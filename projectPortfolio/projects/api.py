c362b823d85800d02434796d9da2ade8016dd68f
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2019-04-13 19:16:01 -0400

    Update api.py

--- projectPortfolio/projects/api.py
+++ projectPortfolio/projects/api.py
@@ -9,23 +9,6 @@ class ProjectViewSet(viewsets.ModelViewSet):
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly,
     ]
-
-    # serializer_class = ProjectSerializer
-    # def get_serializer_class(self):
-    # print("self.action == " + self.action)
-    # if self.action == 'retrieve':
-    # We want the detail page for a project
-    # return ProjectDetailSerializer
-    # We want a list of all the projects
     serializer_class = ProjectSerializer
 
-    # def get_queryset(self):
-    # return self.request.user.projects.all()
-    # if self.action == 'retrieve':
-    # We want the detail page for a project
-    # Project.objects.filter()
-    # We want a list of all the projects
     queryset = Project.objects.all()
-
-    # def perform_create(self, serializer):
-    #     serializer.save(owner=self.request.user)
