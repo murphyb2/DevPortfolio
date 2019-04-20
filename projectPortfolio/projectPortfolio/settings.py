971c67ceb683b5383045ff42e228f144c05c7ee0
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2019-04-20 19:58:29 -0400

    Update settings.py

--- projectPortfolio/projectPortfolio/settings.py
+++ projectPortfolio/projectPortfolio/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'Developmentportfolio-env.ymymdwn3um.us-east-1.elasticbeanstalk.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
