48b6df53d10504220fc58bca45d0399894f136af
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2019-04-21 12:36:34 -0400

    Increased text field max length

--- projectPortfolio/projects/models.py
+++ projectPortfolio/projects/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField(max_length=200, unique=True)
-    description = models.TextField(max_length=1000, blank=True)
+    description = models.TextField(max_length=2000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     cover_image = models.ImageField(upload_to='pictures/', null=True)
     inProgress = models.BooleanField(default=False)
@@ -14,7 +14,7 @@ class Project(models.Model):
 
 
 class About(models.Model):
-    description = models.TextField(max_length=500, blank=True)
+    description = models.TextField(max_length=2000, blank=True)
     prof_pic = models.ImageField(upload_to='pictures/profilePic', null=True)
 
     def __str__(self):
