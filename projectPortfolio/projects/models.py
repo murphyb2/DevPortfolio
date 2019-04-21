617a356588870f7e077c2adccd7d7bca8e61cff3
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2019-04-21 09:27:46 -0400

    Fixed default in inProgress field

--- projectPortfolio/projects/models.py
+++ projectPortfolio/projects/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
     description = models.TextField(max_length=1000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     cover_image = models.ImageField(upload_to='pictures/', null=True)
-    inProgress = models.BooleanField(default=0)
+    inProgress = models.BooleanField(default=False)
 
 
 class About(models.Model):
