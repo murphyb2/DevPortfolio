778e3c6545e4b9a5152176468588b36d8d0cabc6
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2019-04-21 09:14:28 -0400

    Change description field for longer text

--- projectPortfolio/projects/models.py
+++ projectPortfolio/projects/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     name = models.CharField(max_length=200)
     url = models.URLField(max_length=200, unique=True)
-    description = models.CharField(max_length=500, blank=True)
+    description = models.TextField(max_length=1000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     cover_image = models.ImageField(upload_to='pictures/', null=True)
     inProgress = models.BooleanField(default=0)
