ecb9d643bb4ce39d44ed21a699398c9e263e7f38
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2019-05-07 03:35:00 -0400

    Added credit to Hiking Project API

--- projectPortfolio/hiking/src/components/Layout/Footer.js
+++ projectPortfolio/hiking/src/components/Layout/Footer.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class Footer extends Component {
   render() {
     var year = new Date().getFullYear(); //Current Year
     return (
       <footer className="my-3 mx-auto text-center">
-        Bryan Murphy &copy; {year}
+        <div className="col mx-auto">
+          Powered by:{" "}
+          <Link to="https://www.hikingproject.com/data">
+            Hiking Project Data API
+          </Link>
+        </div>
+        <div className="col mx-auto">Bryan Murphy &copy; {year}</div>
       </footer>
     );
   }
