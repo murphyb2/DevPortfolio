e693b7d8923a5e74c022e34517ec4cc0b2f39d11
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2019-07-01 11:51:36 -0400

    Template Rendering
    -Hardcoded template until I can find a better way to process new data each week. Re creating the html file each time is clunky and very unreliable

--- projectPortfolio/subway/views.py
+++ projectPortfolio/subway/views.py
@@ -13,8 +13,8 @@ def index(request):
 
     mp = MapPrep()
     # If the map is not up to date then update it
-    if not mp.map_is_current:
-        mp.update_map()
+    # if not mp.map_is_current:
+    #     mp.update_map()
 
     # If today is not sunday, find the date of the previous sunday
     if dt.datetime.now().weekday() == 6:
@@ -26,5 +26,8 @@ def index(request):
         previous_sunday_date = dt.datetime.now() - dt.timedelta(days=days_since_sunday)
         # Format to YYMMDD
         previous_sunday_date = previous_sunday_date.strftime("%y%m%d")
-
-    return render(request, f'index_{previous_sunday_date}.html')
+    # HARDCODING THE HTML TEMPLATE TO THE LAST SUCCESSFUL BUILD
+    # REBUILDING THE HTML TEMPLATE EACH WEEK IS UNRELIABLE, CLUNKY AND KEEPS BREAKING
+    # READING THE DOCS TO SEE WHAT A BETTER APPROACH WOULD BE
+    return render(request, f'index.html')
+    # return render(request, f'index_{previous_sunday_date}.html')
